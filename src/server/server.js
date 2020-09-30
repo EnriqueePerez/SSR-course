30df9c7e4d5980fe7bee7086c5eb8665fc65a917
EnriqueePerez <enrike_084@hotmail.com>
2020-09-30 17:32:25 -0500

    Webpack and React now handle by Express

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,34 @@
+/* eslint-disable global-require */
 import express from 'express';
-import dotenv from 'dotenv';
+import webpack from 'webpack';
 
-dotenv.config();
+require('dotenv').config();
 
 const app = express();
 
+if (process.env.ENV === 'development') {
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig); //executing webpack with its configurations
+  const serverConfig = { port: process.env.PORT, hot: true }; //declaring the port and enabling hot reload in dev server
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
+}
+
 app.get('*', (req, res) => {
-  res.send({ hello: 'hola' });
+  res.send(`<!DOCTYPE html>
+  <html>
+    <head>
+      <link rel="stylesheet" href="assets/app.css" type="text/css"/>  
+      <title>Platzi Video</title>
+    </head>
+    <body>
+      <div id="app"></div>
+    </body>
+    <script src="assets/app.js" type="text/javascript"></script>
+  </html>`);
 });
 
 app.listen(process.env.PORT, (err) => {
