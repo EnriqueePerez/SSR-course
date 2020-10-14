d767e81ee6351eb5358d431c43be0445520a66f1
EnriqueePerez <enrike_084@hotmail.com>
2020-10-14 08:13:15 -0500

    Server side rendering is now working properly

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,12 @@
+require('ignore-styles');
+
 require('@babel/register')({
   presets: ['@babel/preset-env', '@babel/preset-react'],
 }); //babel register transpiles node files into ES6 syntax
+
+require('asset-require-hook')({
+  extensions: ['jpg', 'png', 'gif'],
+  name: '/assets/[hash].[ext]',
+});
 
 require('./server');
--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,15 @@
 /* eslint-disable global-require */
 import express from 'express';
 import webpack from 'webpack';
+import React from 'react';
+import { renderToString } from 'react-dom/server'; //Funcion para renderear los componentes como string
+import { Provider } from 'react-redux'; //redux
+import { createStore } from 'redux'; //redux
+import { renderRoutes } from 'react-router-config'; //router
+import { StaticRouter } from 'react-router-dom'; //router
+import serverRoutes from '../frontend/routes/serverRoutes';
+import reducer from '../frontend/reducers'; //redux
+import initialState from '../frontend/initialState'; //redux
 
 require('dotenv').config();
 
@@ -17,19 +26,35 @@ if (process.env.ENV === 'development') {
   app.use(webpackHotMiddleware(compiler));
 }
 
-app.get('*', (req, res) => {
-  res.send(`<!DOCTYPE html>
+const setResponse = (html) => {
+  return `<!DOCTYPE html>
   <html>
     <head>
-      <link rel="stylesheet" href="assets/app.css" type="text/css"/>  
+      <link rel="stylesheet" href="assets/app.css" type="text/css"/> 
       <title>Platzi Video</title>
     </head>
     <body>
-      <div id="app"></div>
+      <div id="app">${html}</div>
     </body>
     <script src="assets/app.js" type="text/javascript"></script>
-  </html>`);
-});
+  </html>`;
+};
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>
+  );
+  //con esta funcion preparamos el provider para el redux y el router,
+  //dentro del router colocamos la funcion renderRoutes y le pasamos el archivo de las rutas
+  res.send(setResponse(html));
+};
+
+app.get('*', renderApp);
 
 app.listen(process.env.PORT, (err) => {
   if (err) console.log(err);
